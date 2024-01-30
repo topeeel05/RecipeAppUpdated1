99e8784824a7be3f1b25b60dd42b5f891977dd0d
topeeel05 <topelfungml@gmail.com>
2024-01-30 19:08:40 +0800

    Initial Commit

--- app/src/main/java/com/example/myapplicationnn/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationnn/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.myapplicationnn
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val secondActbutton = findViewById<Button>(R.id.button)
+        secondActbutton.setOnClickListener {
+            val Intent = Intent (this,SecondPage::class.java)
+            startActivity(Intent)
+
+        }
+
+
 
 
     }
