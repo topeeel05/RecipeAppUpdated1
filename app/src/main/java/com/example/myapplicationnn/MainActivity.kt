77ce2218b25f13f1b48a9f16200f5f72953868bb
Christopher Sean Fung <147966949+topeeel05@users.noreply.github.com>
2024-02-02 20:27:17 +0800

    Initial Commit

--- app/src/main/java/com/example/myapplicationnn/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationnn/MainActivity.kt
@@ -4,21 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.myapplicationnn.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val secondActbutton = findViewById<Button>(R.id.button)
-        secondActbutton.setOnClickListener {
-            val Intent = Intent (this,SecondPage::class.java)
-            startActivity(Intent)
-
-        }
-
-
-
+    val btn = findViewById<Button>(R.id.button)
+    btn.setOnClickListener {
+        val Intent = Intent (this,SecondPage::class.java)
+        startActivity(Intent)
 
     }
+
+    }
+
 }
